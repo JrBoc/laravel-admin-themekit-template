f6d9480dde2cd642f626e7183d1ef36051e69d20
John Rhey Boc <johnrheyboc@ed2e.network>
2020-10-16 16:02:32 +0800

    Updated README.md. Added SetupSeeder

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
-Run the Following commands:
-- create your env file
-- composer install
+# Laravel Admin Template
+Build on Laravel 7
+
+Npm dependencies:
+- ThemeKit
+- Jquery
+- Boostrap 4.1
+- Sweetalert
+- Alpinejs
+- FontAwesome 5
+- IconKit
+- Swal2
+- DataTables
+- Select2
+
+Laravel Packages:
+- barryvdh/laravel-ide-helper
+- spatie/laravel-permission
+- livewire/livewire
+- yajra/laravel-datatables-oracle
+
+# Setup
+- Create your .env file
+- php artisan key:generate
+- php artisan migrate
+- php artisan db:seed
 - npm install
 - npm run dev
+- php artisan serve
---
+++ database/seeds/SetupSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class SetupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::create([
+            'name' => 'Super Admin',
+            'guard_name' => 'web',
+        ]);
+
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@test.test',
+            'password' => Hash::make('password'),
+            'status' => 1,
+        ]);
+
+        $user->syncRoles($role);
+    }
+}
